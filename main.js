ed86aa0dca3798cc693a5ef51132fe9ecb488903
Lakia Jones <rachandra_jones@yahoo.com>
2019-03-05 20:25:02 -0600

    last

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
 console.log('Lakia Jones');
+
+const dinosaurs = [
+//loop over dinosaurs//
+//build up a dom string//
+//print domstring to the dom//
+{
+    dinoType: 't-rex',
+    name: 'wrex'
+},
+{
+    dinoType: 'stegosaurus',
+    name: 'stegs'
+},
+{
+    dinoType: 'velociraptor',
+    name: 'raptor'
+},
+];
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+const buildDinosaurs = () => {
+let domstring = '';
+for(let i = 0; i < dinosaurs.length; i++){
+    domstring += `<div class= "dinosaur">`
+    domstring += `<h3>${dinosaurs[i].dinoType}</h3>`;
+    domstring += `<p>${dinosaurs[i].name}</p>`;
+    domstring += `</div>`
+}
+printToDom('dino', domstring);
+};
+
+
+const init = () => {
+buildDinosaurs();
+};
+
+init();
+
+
+
+
+
+
